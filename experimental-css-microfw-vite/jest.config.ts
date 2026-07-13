16118448a23d553d6d73f4d1cd3a43ffbec7b7ee
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-07-13 23:13:51 +0300

    Fix CI

--- experimental-css-microfw-vite/jest.config.ts
+++ experimental-css-microfw-vite/jest.config.ts
@@ -100,11 +100,13 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     "^@/.*\\.(css|less|sass|scss)$": "identity-obj-proxy",
-    "^@/.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.ts",
+    "^@/.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts",
     "^@/(.*)$": "<rootDir>/src/$1",
     "^/public/(.*)$": "<rootDir>/public/$1",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.ts",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
