e55fcc324d37cd26659e9d0d58ac4fcf42a83e97
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-07-14 14:30:12 +0300

    Add experimental-css-on-reactrouter

---
+++ experimental-css-on-reactrouter/app/components/shapes.tsx
@@ -0,0 +1,201 @@
+import "./shape.css"
+
+export function Square() {
+  return <div id="square"></div>
+}
+
+export function Rectangle() {
+  return <div id="rectangle"></div>
+}
+
+export function Circle() {
+  return <div id="circle"></div>
+}
+
+export function Oval() {
+  return <div id="oval"></div>
+}
+
+export function TriangleUp() {
+  return <div id="triangle-up"></div>
+}
+
+export function TriangleDown() {
+  return <div id="triangle-down"></div>
+}
+
+export function TriangleLeft() {
+  return <div id="triangle-left"></div>
+}
+
+export function TriangleRight() {
+  return <div id="triangle-right"></div>
+}
+
+export function TriangleTopLeft() {
+  return <div id="triangle-top-left"></div>
+}
+
+export function TriangleTopRight() {
+  return <div id="triangle-top-right"></div>
+}
+
+export function TriangleBottomLeft() {
+  return <div id="triangle-bottom-left"></div>
+}
+
+export function TriangleBottomRight() {
+  return <div id="triangle-bottom-right"></div>
+}
+
+export function CurvedArrow() {
+  return <div id="curved-arrow"></div>
+}
+
+export function Trapezoid() {
+  return <div id="trapezoid"></div>
+}
+
+export function Parallelogram() {
+  return <div id="parallelogram"></div>
+}
+
+export function StarSix() {
+  return <div id="star-six"></div>
+}
+
+export function StarFive() {
+  return <div id="star-five"></div>
+}
+
+export function Pentagon() {
+  return <div id="pentagon"></div>
+}
+
+export function Hexagon() {
+  return <div id="hexagon"></div>
+}
+
+export function Octagon() {
+  return <div id="octagon"></div>
+}
+
+export function Heart() {
+  return <div id="heart"></div>
+}
+
+export function InfinityShape() {
+  return <div id="infinity"></div>
+}
+
+export function Diamond() {
+  return <div id="diamond"></div>
+}
+
+export function DiamondShield() {
+  return <div id="diamond-shield"></div>
+}
+
+export function DiamondNarrow() {
+  return <div id="diamond-narrow"></div>
+}
+
+export function CutDiamond() {
+  return <div id="cut-diamond"></div>
+}
+
+export function Egg() {
+  return <div id="egg"></div>
+}
+
+export function Pacman() {
+  return <div id="pacman"></div>
+}
+
+export function TalkBubble() {
+  return <div id="talk-bubble"></div>
+}
+
+export function Rss() {
+  return <div id="rss"></div>
+}
+
+export function Burst12() {
+  return <div id="burst-12"></div>
+}
+
+export function Burst8() {
+  return <div id="burst-8"></div>
+}
+
+export function YinYang() {
+  return <div id="yin-yang"></div>
+}
+
+export function BadgeRibbon() {
+  return <div id="badge-ribbon"></div>
+}
+
+export function SpaceInvader() {
+  return <div id="space-invader"></div>
+}
+
+export function Tv() {
+  return <div id="tv"></div>
+}
+
+export function Chevron() {
+  return <div id="chevron"></div>
+}
+
+export function MagnifyingGlass() {
+  return <div id="magnifying-glass"></div>
+}
+
+export function FacebookIcon() {
+  return <div id="facebook-icon"></div>
+}
+
+export function Moon() {
+  return <div id="moon"></div>
+}
+
+export function Flag() {
+  return <div id="flag"></div>
+}
+
+export function Cone() {
+  return <div id="cone"></div>
+}
+
+export function Cross() {
+  return <div id="cross"></div>
+}
+
+export function Base() {
+  return <div id="base"></div>
+}
+
+export function Pointer() {
+  return <div id="pointer"></div>
+}
+
+export function Lock() {
+  return <div id="lock"></div>
+}
+
+export function CurvedCornerBottomLeft() {
+  return <div id="curved-corner-bottom-left"></div>
+}
+
+export function CurvedCornerBottomRight() {
+  return <div id="curved-corner-bottom-right"></div>
+}
+
+export function CurvedCornerTopLeft() {
+  return <div id="curved-corner-top-left"></div>
+}
+
+export function CurvedCornerTopRight() {
+  return <div id="curved-corner-top-right"></div>
+}
