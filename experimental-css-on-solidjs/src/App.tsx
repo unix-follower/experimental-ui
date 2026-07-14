cab43ba3a5414742f236b626aaa2e159aca2c4f2
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-07-14 18:25:30 +0300

    Add experimental-css-on-solidjs

---
+++ experimental-css-on-solidjs/src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from "solid-js"
+import Shape from "./components/Shape"
+
+const App: Component = () => {
+  return (
+    <>
+      <h1>Hello world!!!!</h1>
+      <Shape />
+    </>
+  )
+}
+
+export default App
---
+++ experimental-css-on-solidjs/src/components/shapes.tsx
@@ -0,0 +1,201 @@
+import "./shape.css"
+
+export function Square() {
+  return <div id="square" />
+}
+
+export function Rectangle() {
+  return <div id="rectangle" />
+}
+
+export function Circle() {
+  return <div id="circle" />
+}
+
+export function Oval() {
+  return <div id="oval" />
+}
+
+export function TriangleUp() {
+  return <div id="triangle-up" />
+}
+
+export function TriangleDown() {
+  return <div id="triangle-down" />
+}
+
+export function TriangleLeft() {
+  return <div id="triangle-left" />
+}
+
+export function TriangleRight() {
+  return <div id="triangle-right" />
+}
+
+export function TriangleTopLeft() {
+  return <div id="triangle-top-left" />
+}
+
+export function TriangleTopRight() {
+  return <div id="triangle-top-right" />
+}
+
+export function TriangleBottomLeft() {
+  return <div id="triangle-bottom-left" />
+}
+
+export function TriangleBottomRight() {
+  return <div id="triangle-bottom-right" />
+}
+
+export function CurvedArrow() {
+  return <div id="curved-arrow" />
+}
+
+export function Trapezoid() {
+  return <div id="trapezoid" />
+}
+
+export function Parallelogram() {
+  return <div id="parallelogram" />
+}
+
+export function StarSix() {
+  return <div id="star-six" />
+}
+
+export function StarFive() {
+  return <div id="star-five" />
+}
+
+export function Pentagon() {
+  return <div id="pentagon" />
+}
+
+export function Hexagon() {
+  return <div id="hexagon" />
+}
+
+export function Octagon() {
+  return <div id="octagon" />
+}
+
+export function Heart() {
+  return <div id="heart" />
+}
+
+export function InfinityShape() {
+  return <div id="infinity" />
+}
+
+export function Diamond() {
+  return <div id="diamond" />
+}
+
+export function DiamondShield() {
+  return <div id="diamond-shield" />
+}
+
+export function DiamondNarrow() {
+  return <div id="diamond-narrow" />
+}
+
+export function CutDiamond() {
+  return <div id="cut-diamond" />
+}
+
+export function Egg() {
+  return <div id="egg" />
+}
+
+export function Pacman() {
+  return <div id="pacman" />
+}
+
+export function TalkBubble() {
+  return <div id="talk-bubble" />
+}
+
+export function Rss() {
+  return <div id="rss" />
+}
+
+export function Burst12() {
+  return <div id="burst-12" />
+}
+
+export function Burst8() {
+  return <div id="burst-8" />
+}
+
+export function YinYang() {
+  return <div id="yin-yang" />
+}
+
+export function BadgeRibbon() {
+  return <div id="badge-ribbon" />
+}
+
+export function SpaceInvader() {
+  return <div id="space-invader" />
+}
+
+export function Tv() {
+  return <div id="tv" />
+}
+
+export function Chevron() {
+  return <div id="chevron" />
+}
+
+export function MagnifyingGlass() {
+  return <div id="magnifying-glass" />
+}
+
+export function FacebookIcon() {
+  return <div id="facebook-icon" />
+}
+
+export function Moon() {
+  return <div id="moon" />
+}
+
+export function Flag() {
+  return <div id="flag" />
+}
+
+export function Cone() {
+  return <div id="cone" />
+}
+
+export function Cross() {
+  return <div id="cross" />
+}
+
+export function Base() {
+  return <div id="base" />
+}
+
+export function Pointer() {
+  return <div id="pointer" />
+}
+
+export function Lock() {
+  return <div id="lock" />
+}
+
+export function CurvedCornerBottomLeft() {
+  return <div id="curved-corner-bottom-left" />
+}
+
+export function CurvedCornerBottomRight() {
+  return <div id="curved-corner-bottom-right" />
+}
+
+export function CurvedCornerTopLeft() {
+  return <div id="curved-corner-top-left" />
+}
+
+export function CurvedCornerTopRight() {
+  return <div id="curved-corner-top-right" />
+}
