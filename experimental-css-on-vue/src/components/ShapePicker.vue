faac69a26be1771f81ab657c376b5f26781d0b64
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-07-16 23:15:37 +0300

    Add experimental-css-on-vue

---
+++ experimental-css-on-vue/src/components/ShapePicker.vue
@@ -0,0 +1,135 @@
+<script setup lang="ts">
+import { ref, type Component } from "vue"
+import {
+  Square,
+  Rectangle,
+  Circle,
+  Oval,
+  TriangleUp,
+  TriangleDown,
+  TriangleLeft,
+  TriangleRight,
+  TriangleTopLeft,
+  TriangleTopRight,
+  TriangleBottomLeft,
+  TriangleBottomRight,
+  CurvedArrow,
+  Trapezoid,
+  Parallelogram,
+  StarSix,
+  StarFive,
+  Pentagon,
+  Hexagon,
+  Octagon,
+  Heart,
+  InfinityShape,
+  Diamond,
+  DiamondShield,
+  DiamondNarrow,
+  CutDiamond,
+  Egg,
+  Pacman,
+  TalkBubble,
+  Rss,
+  Burst12,
+  Burst8,
+  YinYang,
+  BadgeRibbon,
+  SpaceInvader,
+  Tv,
+  Chevron,
+  MagnifyingGlass,
+  FacebookIcon,
+  Moon,
+  Flag,
+  Cone,
+  Cross,
+  Base,
+  Pointer,
+  Lock,
+  CurvedCornerBottomLeft,
+  CurvedCornerBottomRight,
+  CurvedCornerTopLeft,
+  CurvedCornerTopRight,
+} from "./shapes"
+
+const shapeMap = new Map<string, Component>([
+  ["Square", Square],
+  ["Rectangle", Rectangle],
+  ["Circle", Circle],
+  ["Oval", Oval],
+  ["TriangleUp", TriangleUp],
+  ["TriangleDown", TriangleDown],
+  ["TriangleLeft", TriangleLeft],
+  ["TriangleRight", TriangleRight],
+  ["TriangleTopLeft", TriangleTopLeft],
+  ["TriangleTopRight", TriangleTopRight],
+  ["TriangleBottomLeft", TriangleBottomLeft],
+  ["TriangleBottomRight", TriangleBottomRight],
+  ["CurvedArrow", CurvedArrow],
+  ["Trapezoid", Trapezoid],
+  ["Parallelogram", Parallelogram],
+  ["StarSix", StarSix],
+  ["StarFive", StarFive],
+  ["Pentagon", Pentagon],
+  ["Hexagon", Hexagon],
+  ["Octagon", Octagon],
+  ["Heart", Heart],
+  ["InfinityShape", InfinityShape],
+  ["Diamond", Diamond],
+  ["DiamondShield", DiamondShield],
+  ["DiamondNarrow", DiamondNarrow],
+  ["CutDiamond", CutDiamond],
+  ["Egg", Egg],
+  ["Pacman", Pacman],
+  ["TalkBubble", TalkBubble],
+  ["Rss", Rss],
+  ["Burst12", Burst12],
+  ["Burst8", Burst8],
+  ["YinYang", YinYang],
+  ["BadgeRibbon", BadgeRibbon],
+  ["SpaceInvader", SpaceInvader],
+  ["Tv", Tv],
+  ["Chevron", Chevron],
+  ["MagnifyingGlass", MagnifyingGlass],
+  ["FacebookIcon", FacebookIcon],
+  ["Moon", Moon],
+  ["Flag", Flag],
+  ["Cone", Cone],
+  ["Cross", Cross],
+  ["Base", Base],
+  ["Pointer", Pointer],
+  ["Lock", Lock],
+  ["CurvedCornerBottomLeft", CurvedCornerBottomLeft],
+  ["CurvedCornerBottomRight", CurvedCornerBottomRight],
+  ["CurvedCornerTopLeft", CurvedCornerTopLeft],
+  ["CurvedCornerTopRight", CurvedCornerTopRight],
+])
+
+const shapeNames = Array.from(shapeMap.keys())
+
+const shapeComponents = ref<Component[]>([])
+
+function handleShapeChange(event: Event) {
+  const select = event.target as HTMLSelectElement
+  const selectedOptions = Array.from(select.selectedOptions).map((option) => option.value)
+  const components: Component[] = []
+  for (const option of selectedOptions) {
+    const component = shapeMap.get(option)
+    if (component) {
+      components.push(component)
+    }
+  }
+  shapeComponents.value = components
+}
+</script>
+
+<template>
+  <div>
+    <label for="selectShape">Choose shapes:</label>
+    <select id="selectShape" multiple size="4" @change="handleShapeChange">
+      <option v-for="name in shapeNames" :key="name" :value="name">{{ name }}</option>
+    </select>
+    <component :is="comp" v-for="(comp, index) in shapeComponents" :key="index" />
+  </div>
+</template>
---
+++ experimental-css-on-vue/src/components/shapes.ts
@@ -0,0 +1,109 @@
+import "./shape.css"
+import { h } from "vue"
+
+export const Square = () => h("div", { id: "square", key: "square" })
+
+export const Rectangle = () => h("div", { id: "rectangle", key: "rectangle" })
+
+export const Circle = () => h("div", { id: "circle", key: "circle" })
+
+export const Oval = () => h("div", { id: "oval", key: "oval" })
+
+export const TriangleUp = () => h("div", { id: "triangle-up", key: "triangle-up" })
+
+export const TriangleDown = () => h("div", { id: "triangle-down", key: "triangle-down" })
+
+export const TriangleLeft = () => h("div", { id: "triangle-left", key: "triangle-left" })
+
+export const TriangleRight = () => h("div", { id: "triangle-right", key: "triangle-right" })
+
+export const TriangleTopLeft = () => h("div", { id: "triangle-top-left", key: "triangle-top-left" })
+
+export const TriangleTopRight = () =>
+  h("div", { id: "triangle-top-right", key: "triangle-top-right" })
+
+export const TriangleBottomLeft = () =>
+  h("div", { id: "triangle-bottom-left", key: "triangle-bottom-left" })
+
+export const TriangleBottomRight = () =>
+  h("div", { id: "triangle-bottom-right", key: "triangle-bottom-right" })
+
+export const CurvedArrow = () => h("div", { id: "curved-arrow", key: "curved-arrow" })
+
+export const Trapezoid = () => h("div", { id: "trapezoid", key: "trapezoid" })
+
+export const Parallelogram = () => h("div", { id: "parallelogram", key: "parallelogram" })
+
+export const StarSix = () => h("div", { id: "star-six", key: "star-six" })
+
+export const StarFive = () => h("div", { id: "star-five", key: "star-five" })
+
+export const Pentagon = () => h("div", { id: "pentagon", key: "pentagon" })
+
+export const Hexagon = () => h("div", { id: "hexagon", key: "hexagon" })
+
+export const Octagon = () => h("div", { id: "octagon", key: "octagon" })
+
+export const Heart = () => h("div", { id: "heart", key: "heart" })
+
+export const InfinityShape = () => h("div", { id: "infinity", key: "infinity" })
+
+export const Diamond = () => h("div", { id: "diamond", key: "diamond" })
+
+export const DiamondShield = () => h("div", { id: "diamond-shield", key: "diamond-shield" })
+
+export const DiamondNarrow = () => h("div", { id: "diamond-narrow", key: "diamond-narrow" })
+
+export const CutDiamond = () => h("div", { id: "cut-diamond", key: "cut-diamond" })
+
+export const Egg = () => h("div", { id: "egg", key: "egg" })
+
+export const Pacman = () => h("div", { id: "pacman", key: "pacman" })
+
+export const TalkBubble = () => h("div", { id: "talk-bubble", key: "talk-bubble" })
+
+export const Rss = () => h("div", { id: "rss", key: "rss" })
+
+export const Burst12 = () => h("div", { id: "burst-12", key: "burst-12" })
+
+export const Burst8 = () => h("div", { id: "burst-8", key: "burst-8" })
+
+export const YinYang = () => h("div", { id: "yin-yang", key: "yin-yang" })
+
+export const BadgeRibbon = () => h("div", { id: "badge-ribbon", key: "badge-ribbon" })
+
+export const SpaceInvader = () => h("div", { id: "space-invader", key: "space-invader" })
+
+export const Tv = () => h("div", { id: "tv", key: "tv" })
+
+export const Chevron = () => h("div", { id: "chevron", key: "chevron" })
+
+export const MagnifyingGlass = () => h("div", { id: "magnifying-glass", key: "magnifying-glass" })
+
+export const FacebookIcon = () => h("div", { id: "facebook-icon", key: "facebook-icon" })
+
+export const Moon = () => h("div", { id: "moon", key: "moon" })
+
+export const Flag = () => h("div", { id: "flag", key: "flag" })
+
+export const Cone = () => h("div", { id: "cone", key: "cone" })
+
+export const Cross = () => h("div", { id: "cross", key: "cross" })
+
+export const Base = () => h("div", { id: "base", key: "base" })
+
+export const Pointer = () => h("div", { id: "pointer", key: "pointer" })
+
+export const Lock = () => h("div", { id: "lock", key: "lock" })
+
+export const CurvedCornerBottomLeft = () =>
+  h("div", { id: "curved-corner-bottom-left", key: "curved-corner-bottom-left" })
+
+export const CurvedCornerBottomRight = () =>
+  h("div", { id: "curved-corner-bottom-right", key: "curved-corner-bottom-right" })
+
+export const CurvedCornerTopLeft = () =>
+  h("div", { id: "curved-corner-top-left", key: "curved-corner-top-left" })
+
+export const CurvedCornerTopRight = () =>
+  h("div", { id: "curved-corner-top-right", key: "curved-corner-top-right" })
