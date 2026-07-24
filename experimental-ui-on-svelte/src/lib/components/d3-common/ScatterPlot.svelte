2df8254f929f64c7ac4d43c8bdfbd2e4cbb3b48e
Artsem Nikitsenka <artsem_nikitsenka@epam.com>
2026-07-24 15:55:15 +0300

    Add D3 scatter plot for delaney-processed dataset

---
+++ experimental-ui-on-svelte/src/lib/components/d3-common/ScatterPlot.svelte
@@ -0,0 +1,81 @@
+<script>
+	import * as d3 from "d3"
+
+	let { data, xProperty, yProperty, margin } = $props()
+
+	let chartWidth = $state(400)
+	let chartHeight = 400
+
+	let innerChartWidth = $derived(chartWidth - margin.left - margin.right)
+	let innerChartHeight = $derived(chartHeight - margin.top - margin.bottom)
+
+	let xScale = $derived(
+		d3
+			.scaleLinear()
+			.domain([
+				d3.min(data, (row) => +row[xProperty]),
+				d3.max(data, (row) => +row[xProperty]),
+			])
+			.range([0, innerChartWidth])
+			.nice(),
+	)
+	let yScale = $derived(
+		d3
+			.scaleLinear()
+			.domain([
+				d3.min(data, (row) => +row[yProperty]),
+				d3.max(data, (row) => +row[yProperty]),
+			])
+			.range([innerChartHeight, 0])
+			.nice(),
+	)
+
+	let xTicks = $derived(xScale.ticks())
+	let yTicks = $derived(yScale.ticks())
+</script>
+
+<div bind:clientWidth={chartWidth}>
+	<svg width={chartWidth} height={chartHeight} role="img" aria-label="Scatter plot of {yProperty} vs {xProperty}">
+		<g style="transform: translate({margin.left}px, {margin.top}px)">
+			<g style="transform: translate(0, {innerChartHeight}px)">
+				<line x1="0" y1="0" x2={innerChartWidth} y2="0" stroke="black" stroke-width="4px" />
+				{#each xTicks as tick (tick)}
+					<text x={xScale(tick)} y={25}>{tick}</text>
+				{/each}
+				<text x={innerChartWidth / 2} y="50px">{xProperty}</text>
+			</g>
+			<g>
+				<line x1="0" y1="0" x2="0" y2={innerChartHeight} stroke="black" stroke-width="4px" />
+				{#each yTicks as tick (tick)}
+					<text x="-35px" y={yScale(tick)}>{tick}</text>
+				{/each}
+				<g style="transform: translate(-65px, {innerChartHeight / 2}px) rotate(-90deg)">
+					<text>{yProperty}</text>
+				</g>
+			</g>
+			{#each data as row (row["Compound ID"])}
+				<circle
+					cx={xScale(+row[xProperty])}
+					cy={yScale(+row[yProperty])}
+					fill="blue"
+					r="8px"
+				/>
+			{/each}
+		</g>
+	</svg>
+</div>
+
+<style>
+	svg {
+		background-color: white;
+	}
+
+	text {
+		dominant-baseline: middle;
+		text-anchor: middle;
+	}
+
+	circle {
+		transition: all 500ms ease;
+	}
+</style>
---
+++ experimental-ui-on-svelte/src/routes/chemistry/molnet-delaney-processed/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { onMount } from "svelte"
+	import * as d3 from "d3"
+
+	import ScatterPlot from "$lib/components/d3-common/ScatterPlot.svelte"
+
+	let data = $state([])
+	let xProperty = $state("Number of Rings")
+	let yProperty = $state("Polar Surface Area")
+
+	onMount(async () => {
+		data = await d3.csv(
+			"https://raw.githubusercontent.com/unix-follower/molecule-analysis/main/data/delaney-processed.csv",
+			d3.autoType,
+		)
+	})
+
+	const margin = {
+		top: 50,
+		right: 50,
+		bottom: 70,
+		left: 95,
+	}
+</script>
+
+<div>
+	<label>
+		Independent variable
+		<select bind:value={xProperty}>
+			<option value="Number of Rings">Number of Rings</option>
+			<option value="Number of Rotatable Bonds">Number of Rotatable Bonds</option>
+		</select>
+	</label>
+
+	<label>
+		Dependent variable
+		<select bind:value={yProperty}>
+			<option value="Polar Surface Area">Polar Surface Area</option>
+			<option value="measured log solubility in mols per litre">Measured Log Solubility in Mols per Litre</option>
+		</select>
+	</label>
+</div>
+
+<ScatterPlot {data} {xProperty} {yProperty} {margin} />
